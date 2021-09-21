e7a2e6c1079b886ec79422876cfaf796c4b8dfa6
udit-gulati <uditgulati6@gmail.com>
2021-09-21 14:13:43 +0530

    add network maps to constants

--- perppy/utils/constants.py
+++ perppy/utils/constants.py
@@ -1,5 +1,4 @@
 
-
 ETH_DECIMALS = 10**18
 
 PRODUCTION_METADATA_URL = "https://metadata.perp.exchange/production.json"
@@ -10,13 +9,26 @@ xDAI_URL          = "https://rpc.xdaichain.com/"
 RINKEBY_URL       = "https://rinkeby.infura.io/v3/" + INFURA_PROJECT_ID
 HOMESTEAD_URL     = "https://mainnet.infura.io/v3/" + INFURA_PROJECT_ID
 
+NETWORK_NAMES = ["xDai", "Rinkeby", "Homestead"]
+LAYER_MAP = {
+    "layer1": {
+        "production": "Homestead",
+        "staging": "Rinkeby",
+    },
+    "layer2": {
+        "production": "xDai",
+        "staging": "xDai",
+    }
+}
+
+NETWORK_MAP = {
+    "xDai": xDAI_URL,
+    "Rinkeby": RINKEBY_URL,
+    "Homestead": HOMESTEAD_URL,
+}
+
 
 def get_network_url(network_name: str) -> str:
-    network_map = {
-        "xDai": xDAI_URL,
-        "Rinkeby": RINKEBY_URL,
-        "Homestead": HOMESTEAD_URL,
-    }
-    if network_name not in network_map:
-        raise ValueError(f"Invalid network name {network_name}, available: {list(network_map.keys())}")
-    return network_map[network_name]
+    if network_name not in NETWORK_MAP:
+        raise ValueError(f"Invalid network name {network_name}, available: {list(NETWORK_MAP.keys())}")
+    return NETWORK_MAP[network_name]
