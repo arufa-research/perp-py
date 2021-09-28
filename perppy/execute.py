44f2fcf1a1adaeb9b6d70cfc4f31b5e8ba358c9a
udit-gulati <uditgulati6@gmail.com>
2021-09-29 04:07:13 +0530

    fix functions signature errors

--- perppy/execute.py
+++ perppy/execute.py
@@ -1,16 +1,32 @@
 
 
-def open_position(amm_addr: str, side: int, quote_asset_amount: float, leverage: int, base_asset_amount_limit: float, gasLimit: float = 0.):
+def open_position(
+    amm_addr: str,
+    side: int,
+    quote_asset_amount: float,
+    leverage: int,
+    base_asset_amount_limit: float,
+    gasLimit: float = 0.,
+):
     pass
 
 
-def close_position(amm_addr: str, quote_asset_amount_limit: float):
+def close_position(
+    amm_addr: str,
+    quote_asset_amount_limit: float,
+):
     pass
 
 
-def add_margin(amm_addr: str, margin: float):
+def add_margin(
+    amm_addr: str,
+    margin: float,
+):
     pass
 
 
-def remove_margin(amm_addr: str, margin: float):
+def remove_margin(
+    amm_addr: str,
+    margin: float,
+):
     pass
